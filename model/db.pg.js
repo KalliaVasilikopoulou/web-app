aab2cd392e01c6c175726af76bc03f28d6aca0a9
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-07-06 18:15:10 +0300

    update on db

--- model/db.pg.js
+++ model/db.pg.js
@@ -3,14 +3,15 @@ const { Client } = require('pg');
 const knex = require('knex');
 
 const client = new Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    // connectionString: process.env.DATABASE_URL,
-    idleTimeoutMillis: 0,
-    connectionTimeoutMillis: 0
+    // user: process.env.PG_USER,
+    // host: process.env.PG_HOST,
+    // database: process.env.PG_DATABASE,
+    // password: process.env.PG_PASSWORD,
+    // port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+    // idleTimeoutMillis: 0,
+    // connectionTimeoutMillis: 0
 });
 
 client.connect((err) => {
