866b9fd82f4268f69cc05f4696f177024e765925
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-07-06 21:46:55 +0300

    Third change at database_url of db.pg.js

--- model/db.pg.js
+++ model/db.pg.js
@@ -7,8 +7,8 @@ const client = new Client({
     // host: process.env.PG_HOST,
     // database: process.env.PG_DATABASE,
     // password: benzaiten2014,
-    // port: 1000,
-    connectionString: https://data.heroku.com/dataclips/myuyvblbtehgbranaxcpokofmrwq,
+    // port: '1000',
+    connectionString: 'https://data.heroku.com/dataclips/myuyvblbtehgbranaxcpokofmrwq',
     ssl: { rejectUnauthorized: false }
     // idleTimeoutMillis: 0,
     // connectionTimeoutMillis: 0
